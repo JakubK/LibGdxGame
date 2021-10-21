4e831075e01afd3aa468f0f620c80fc72b3698df
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-21 22:39:20 +0200

    Hardcode preferred window resolution and disable resizing

--- desktop/src/com/gdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.gdx.game.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 768;
+		config.resizable = false;
 		new LwjglApplication(new Game(), config);
 	}
 }
