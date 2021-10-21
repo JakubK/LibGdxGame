e3ddf5d63698957eb104cd7e971e3cddee294e2a
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-21 23:03:59 +0200

    Prepare working samples of rendering Image, Rect and Text

--- core/src/com/gdx/game/Game.java
+++ core/src/com/gdx/game/Game.java
@@ -1,25 +1,43 @@
 package com.gdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	BitmapFont font;
+	ShapeRenderer shapeRenderer;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		shapeRenderer = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
+		batch.begin();//image
 		batch.draw(img, 0, 0);
+		batch.end();
+
+		batch.begin();//rect
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.BLACK);
+		shapeRenderer.rect(0, 0, 50, 50);
+		shapeRenderer.end();
+		batch.end();
+
+		batch.begin();//text
+		font.draw(batch, "Hello LibGDX", 10,20);
 		batch.end();
 	}
 	
@@ -27,5 +45,7 @@ public class Game extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		font.dispose();
+		shapeRenderer.dispose();
 	}
 }
