14702165d1ffab9fa72da9966ba3c056dd7c8cfb
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-22 20:35:26 +0200

    Extend GameConstants with Player speed and dimensions

--- core/src/com/gdx/game/GameConstants.java
+++ core/src/com/gdx/game/GameConstants.java
@@ -3,4 +3,7 @@ package com.gdx.game;
 public final class GameConstants {
     public static int SCREEN_WIDTH = 1024;
     public static int SCREEN_HEIGHT = 768;
+    public static int PLAYER_SPEED = 10;
+    public static int PLAYER_WIDTH = 20;
+    public static int PLAYER_HEIGHT = 20;
 }
