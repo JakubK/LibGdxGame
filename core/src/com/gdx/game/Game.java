2a51e7472ed76dd4dfd648d448c536c83dbc7f04
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-22 20:17:00 +0200

    Comment samples and mount BottomBar in Game

--- core/src/com/gdx/game/Game.java
+++ core/src/com/gdx/game/Game.java
@@ -14,31 +14,39 @@ public class Game extends ApplicationAdapter {
 	Texture img;
 	BitmapFont font;
 	ShapeRenderer shapeRenderer;
+
+
+	BottomBar bottomBar;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		font = new BitmapFont();
 		shapeRenderer = new ShapeRenderer();
+
+		bottomBar = new BottomBar();
+		bottomBar.create();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();//image
-		batch.draw(img, 0, 0);
-		batch.end();
-
-		batch.begin();//rect
-		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-		shapeRenderer.setColor(Color.BLACK);
-		shapeRenderer.rect(0, 0, 50, 50);
-		shapeRenderer.end();
-		batch.end();
-
-		batch.begin();//text
-		font.draw(batch, "Hello LibGDX", 10,20);
-		batch.end();
+//		batch.begin();//image
+//		batch.draw(img, 0, 0);
+//		batch.end();
+//
+//		batch.begin();//rect
+//		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+//		shapeRenderer.setColor(Color.BLACK);
+//		shapeRenderer.rect(0, 0, 50, 50);
+//		shapeRenderer.end();
+//		batch.end();
+//
+//		batch.begin();//text
+//		font.draw(batch, "Hello LibGDX", 10,20);
+//		batch.end();
+		bottomBar.render(batch);
 	}
 	
 	@Override
@@ -47,5 +55,7 @@ public class Game extends ApplicationAdapter {
 		img.dispose();
 		font.dispose();
 		shapeRenderer.dispose();
+
+		bottomBar.dispose();
 	}
 }
