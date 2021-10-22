efe2e4270519639c92dd83bf9a0017a5946a7e4e
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-22 20:15:57 +0200

    Introduce static class with Game Constants including dimensions

---
+++ core/src/com/gdx/game/GameConstants.java
@@ -0,0 +1,6 @@
+package com.gdx.game;
+
+public final class GameConstants {
+    public static int SCREEN_WIDTH = 1024;
+    public static int SCREEN_HEIGHT = 768;
+}
--- desktop/src/com/gdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/gdx/game/desktop/DesktopLauncher.java
@@ -3,12 +3,13 @@ package com.gdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.gdx.game.Game;
+import com.gdx.game.GameConstants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
-		config.height = 768;
+		config.width = GameConstants.SCREEN_WIDTH;
+		config.height = GameConstants.SCREEN_HEIGHT;
 		config.resizable = false;
 		new LwjglApplication(new Game(), config);
 	}
