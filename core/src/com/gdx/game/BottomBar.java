ecec1b36f99579717ce1ef7bb3430a63cdde79b9
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-23 17:36:50 +0200

    Add points dependency for BottomBar

--- core/src/com/gdx/game/BottomBar.java
+++ core/src/com/gdx/game/BottomBar.java
@@ -10,10 +10,16 @@ public class BottomBar {
     ShapeRenderer renderer;
     BitmapFont font;
 
+    int points = 0;
+
     public void create()
     {
         renderer = new ShapeRenderer();
         font = new BitmapFont();
+    }
+
+    public void addPoint() {
+        points++;
     }
 
     public void render(SpriteBatch batch)
@@ -25,12 +31,11 @@ public class BottomBar {
         renderer.rect(0, 0, GameConstants.SCREEN_WIDTH, 50);
         renderer.end();
 
-
         batch.end();
 
         batch.begin();
         font.setColor(Color.BLACK);
-        font.draw(batch,"Points: 0", 30, 30);
+        font.draw(batch,"Points: " + points, 30, 30);
 
         font.draw(batch, "Next element:", GameConstants.SCREEN_WIDTH - 200, 30);
 
